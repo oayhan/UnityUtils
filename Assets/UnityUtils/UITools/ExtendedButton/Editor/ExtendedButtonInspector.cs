32fb8fefaf12499d5cd355afcc8ae281d774ef52
Ömer Ayhan <ayhanomer@gmail.com>
2019-01-04 13:27:22 +0300

    extended button modifications and editor

--- Assets/UnityUtils/UITools/ExtendedButton/Editor/ExtendedButtonInspector.cs
+++ Assets/UnityUtils/UITools/ExtendedButton/Editor/ExtendedButtonInspector.cs
@@ -1,9 +1,119 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+[CustomEditor(typeof(ExtendedButton))]
+[CanEditMultipleObjects]
 public class ExtendedButtonInspector : Editor
 {
-    
+    public SerializedProperty
+        TransitionProperty,
+        NormalColorProperty,
+        HighlightedColorProperty,
+        PressedColorProperty,
+        DisabledColorProperty,
+        FadeTimeProperty,
+        DisableAffectsTextProperty,
+        NormalTextColorProperty,
+        DisabledTextColorProperty,
+        NormalSpriteProperty,
+        HighlightedSpriteProperty,
+        PressedSpriteProperty,
+        DisabledSpriteProperty,
+        PressClipProperty,
+        HighlightClipProperty,
+        DisabledPressClipProperty,
+        HighlightedScaleProperty,
+        PressedScaleProperty,
+        ScaleTimeProperty,
+        OnHighlightProperty,
+        OnPressProperty;
+
+    //cache all properties
+    private void OnEnable()
+    {
+        TransitionProperty = serializedObject.FindProperty("Transition");
+        
+        NormalColorProperty = serializedObject.FindProperty("NormalColor");
+        HighlightedColorProperty = serializedObject.FindProperty("HighlightedColor");
+        PressedColorProperty = serializedObject.FindProperty("PressedColor");
+        DisabledColorProperty = serializedObject.FindProperty("DisabledColor");
+        FadeTimeProperty = serializedObject.FindProperty("FadeTime");
+        
+        DisableAffectsTextProperty = serializedObject.FindProperty("DisableAffectsText");
+        NormalTextColorProperty = serializedObject.FindProperty("NormalTextColor");
+        DisabledTextColorProperty = serializedObject.FindProperty("DisabledTextColor");
+        
+        NormalSpriteProperty = serializedObject.FindProperty("NormalSprite");
+        HighlightedSpriteProperty = serializedObject.FindProperty("HighlightedSprite");
+        PressedSpriteProperty = serializedObject.FindProperty("PressedSprite");
+        DisabledSpriteProperty = serializedObject.FindProperty("DisabledSprite");
+        
+        PressClipProperty = serializedObject.FindProperty("PressClip");
+        HighlightClipProperty = serializedObject.FindProperty("HighlightClip");
+        DisabledPressClipProperty = serializedObject.FindProperty("DisabledPressClip");
+        
+        HighlightedScaleProperty = serializedObject.FindProperty("HighlightedScale");
+        PressedScaleProperty = serializedObject.FindProperty("PressedScale");
+        ScaleTimeProperty = serializedObject.FindProperty("ScaleTime");
+        
+        OnHighlightProperty = serializedObject.FindProperty("OnHighlight");
+        OnPressProperty = serializedObject.FindProperty("OnPress");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        //transition type
+        EditorGUILayout.PropertyField(TransitionProperty);
+
+        //get value of transition type
+        ExtendedButton.ExtendedButtonTransitionType transitionType =
+            (ExtendedButton.ExtendedButtonTransitionType) TransitionProperty.intValue;
+
+        //display transition values according to type
+        switch (transitionType)
+        {
+            case ExtendedButton.ExtendedButtonTransitionType.Color:
+                EditorGUILayout.PropertyField(NormalColorProperty, new GUIContent("Normal Color"));
+                EditorGUILayout.PropertyField(HighlightedColorProperty, new GUIContent("Highlighted Color"));
+                EditorGUILayout.PropertyField(PressedColorProperty, new GUIContent("Pressed Color"));
+                EditorGUILayout.PropertyField(DisabledColorProperty, new GUIContent("Disabled Color"));
+                EditorGUILayout.PropertyField(FadeTimeProperty, new GUIContent("Fade Time"));
+                break;
+            case ExtendedButton.ExtendedButtonTransitionType.Sprite:
+                EditorGUILayout.PropertyField(NormalSpriteProperty, new GUIContent("Normal Sprite"));
+                EditorGUILayout.PropertyField(HighlightedSpriteProperty, new GUIContent("Highlighted Sprite"));
+                EditorGUILayout.PropertyField(PressedSpriteProperty, new GUIContent("Pressed Sprite"));
+                EditorGUILayout.PropertyField(DisabledSpriteProperty, new GUIContent("Disabled Sprite"));
+                break;
+        }
+        
+        //text color values
+        EditorGUILayout.PropertyField(DisableAffectsTextProperty, new GUIContent("Disable Affects Text"));
+        if (DisableAffectsTextProperty.boolValue)
+        {
+            EditorGUILayout.PropertyField(NormalTextColorProperty, new GUIContent("Normal Text Color"));
+            EditorGUILayout.PropertyField(DisabledTextColorProperty, new GUIContent("Disabled Text Color"));
+        }
+
+        //audio clips
+        EditorGUILayout.PropertyField(PressClipProperty, new GUIContent("Press Clip"));
+        EditorGUILayout.PropertyField(HighlightClipProperty, new GUIContent("Highlight Clip"));
+        EditorGUILayout.PropertyField(DisabledPressClipProperty, new GUIContent("Disabled Press Clip"));
+        
+        //scale changes
+        EditorGUILayout.PropertyField(HighlightedScaleProperty, new GUIContent("Highlighted Scale"));
+        EditorGUILayout.PropertyField(PressedScaleProperty, new GUIContent("Pressed Scale"));
+        EditorGUILayout.PropertyField(ScaleTimeProperty, new GUIContent("Scale Time"));
+
+        //UnityEvents
+        EditorGUILayout.PropertyField(OnHighlightProperty, new GUIContent("On Highlight"));
+        EditorGUILayout.PropertyField(OnPressProperty, new GUIContent("On Press"));
+        
+        serializedObject.ApplyModifiedProperties();
+    }
 }
--- Assets/UnityUtils/UITools/ExtendedButton/ExtendedButton.cs
+++ Assets/UnityUtils/UITools/ExtendedButton/ExtendedButton.cs
@@ -60,19 +60,19 @@ public class ExtendedButton : MonoBehaviour, IPointerClickHandler, IPointerDownH
     public Vector3 HighlightedScale = Vector3.one;
     [Tooltip("Scale of button when pressed.")]
     public Vector3 PressedScale = Vector3.one;
+    [Tooltip("How long it takes to finish scale change.")]
+    [Range(0, 1)]
+    public float ScaleTime = 0.1f;
     
-    [Header("Position Offsets")]
-    [Tooltip("Positional offset of button when highlighted.")]
-    public Vector3 HighlightedOffset = Vector3.zero;
-    [Tooltip("Positional offset of button when pressed.")]
-    public Vector3 PressedOffset = Vector3.zero;
-
     [Header("Events")]
     public UnityEvent OnHighlight;
     public UnityEvent OnPress;
 
+    //click and hover events doesn't register when disabled
     public bool IsDisabled { get; private set; }
+    //is button currently hovered over
     public bool IsHovered { get; private set; }
+    //current state of button (normal, hovered, pressed or disabled)
     public ExtendedButtonState CurrentState { get; private set; }
 
     //size constants
@@ -83,15 +83,31 @@ public class ExtendedButton : MonoBehaviour, IPointerClickHandler, IPointerDownH
     private const string ButtonText = "Button";
     private static readonly Color TextColor = Color.black;
     
+    //cached components
     private Image buttonImage;
     private TMP_Text buttonText;
+    private RectTransform rectTransform;
+    
+    //cached values
+    private Vector3 defaultScale;
+    private Vector3 defaultOffset;
     
     private void Start()
     {
+        //cache components
         buttonImage = GetComponent<Image>();
         buttonText = GetComponentInChildren<TMP_Text>();
+        rectTransform = GetComponent<RectTransform>();
+        
+        //cache default values
+        defaultScale = rectTransform.localScale;
+        defaultOffset = rectTransform.anchoredPosition3D;
     }
     
+    /// <summary>
+    /// Enables/disables the button.
+    /// </summary>
+    /// <param name="toggle"></param>
     public void ToggleDisable(bool toggle)
     {
         IsDisabled = toggle;
@@ -101,75 +117,119 @@ public class ExtendedButton : MonoBehaviour, IPointerClickHandler, IPointerDownH
         SetButtonStyle();
     }
     
+    /// <summary>
+    /// Implemented for IPointerClickHandler interface. Deals with button clicks.
+    /// </summary>
+    /// <param name="eventData"></param>
     public void OnPointerClick(PointerEventData eventData)
     {
+        //if the button is disabled
         if (IsDisabled)
         {
+            //play disabled clip
             if(DisabledPressClip != null)
                 DisabledPressClip.PlayOneShot();
             
+            //exit without doing anything
             return;
         }
 
+        //play normal press clip
         if (PressClip != null)
             PressClip.PlayOneShot();
         
+        //call OnPress UnityEvent
         if(OnPress != null)
             OnPress.Invoke();
     }
 
+    /// <summary>
+    /// Implemented for IPointerDownHandler interface. Used for setting button style when button is pressed down.
+    /// </summary>
+    /// <param name="eventData"></param>
     public void OnPointerDown(PointerEventData eventData)
     {
+        //if button is disabled, exit function
         if (IsDisabled)
             return;
         
+        //set new state
         CurrentState = ExtendedButtonState.Pressed;
         
+        //set button style
         SetButtonStyle();
     }
 
+    /// <summary>
+    /// Implemented for IPointerUpHandler interface. Used for setting button style when button press is finished.
+    /// </summary>
+    /// <param name="eventData"></param>
     public void OnPointerUp(PointerEventData eventData)
     {
+        //if button is disabled, exit function
         if (IsDisabled)
             return;
 
-        if (!IsHovered)
-            CurrentState = ExtendedButtonState.Normal;
-        else
-            CurrentState = ExtendedButtonState.Highlighted;
+        //if the mouse is still over the button set the state to Highlighted, else set to Normal
+        CurrentState = IsHovered ? ExtendedButtonState.Highlighted : ExtendedButtonState.Normal;
         
+        //set button style
         SetButtonStyle();
     }
 
+    /// <summary>
+    /// Implemented for IPointerEnterHandler interface. Used for setting button style when button is hovered.
+    /// </summary>
+    /// <param name="eventData"></param>
     public void OnPointerEnter(PointerEventData eventData)
     {
+        //if button is disabled, exit function
         if (IsDisabled)
             return;
 
+        //set flag and state
         IsHovered = true;
         CurrentState = ExtendedButtonState.Highlighted;
         
+        //play audio clip
         if(HighlightClip != null)
             HighlightClip.PlayOneShot();
         
+        //call OnHighlight UnityEvent
+        if(OnHighlight != null)
+            OnHighlight.Invoke();
+        
+        //set button style
         SetButtonStyle();
     }
     
+    /// <summary>
+    /// Implemented for IPointerExitHandler interface. Used for setting button style when button hover is finished.
+    /// </summary>
+    /// <param name="eventData"></param>
     public void OnPointerExit(PointerEventData eventData)
     {
+        //if button is disabled, exit function
         if (IsDisabled)
             return;
 
+        //set flag and state
         IsHovered = false;
         CurrentState = ExtendedButtonState.Normal;
         
+        //set button style
         SetButtonStyle();
     }
 
+    /// <summary>
+    /// Sets visual style of button depending on Transition Type and current Button State.
+    /// </summary>
     private void SetButtonStyle()
     {
+        //if transition type is Color
         if (Transition == ExtendedButtonTransitionType.Color)
         {
+            //change Image color to state assigned values using fade time
             switch (CurrentState)
             {
                 case ExtendedButtonState.Normal:
@@ -186,8 +246,10 @@ public class ExtendedButton : MonoBehaviour, IPointerClickHandler, IPointerDownH
                     break;
             }
         }
+        //if transition type is Sprite
         else if (Transition == ExtendedButtonTransitionType.Sprite)
         {
+            //change Image sprite to the state assigned values
             switch (CurrentState)
             {
                 case ExtendedButtonState.Normal:
@@ -205,8 +267,10 @@ public class ExtendedButton : MonoBehaviour, IPointerClickHandler, IPointerDownH
             }
         }
 
+        //if disabling the button is set to affect the text
         if (DisableAffectsText)
         {
+            //change text color depending on button state
             if (CurrentState == ExtendedButtonState.Normal)
             {
                 buttonText.color = NormalTextColor;
@@ -216,35 +280,83 @@ public class ExtendedButton : MonoBehaviour, IPointerClickHandler, IPointerDownH
                 buttonText.color = DisabledTextColor;
             }
         }
-        
+
+        //stop previous Coroutines and restart with new state 
+        StopAllCoroutines();
+        StartCoroutine(ChangeScale());
     }
 
+    /// <summary>
+    /// Changes button scale depending on CurrentState in ScaleTime.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ChangeScale()
+    {
+        Vector3 targetScale = Vector3.one;
+        Vector3 currentScale = rectTransform.localScale;
+        
+        switch (CurrentState)
+        {
+            case ExtendedButtonState.Normal:
+            case ExtendedButtonState.Disabled:
+                targetScale = defaultScale;
+                break;
+            case ExtendedButtonState.Highlighted:
+                targetScale = HighlightedScale;
+                break;
+            case ExtendedButtonState.Pressed:
+                targetScale = PressedScale;
+                break;
+        }
+        
+        float elapsedTime = 0;
+        while (elapsedTime <= ScaleTime)
+        {
+            rectTransform.localScale = Vector3.Lerp(currentScale, targetScale, Mathf.Clamp01(1 - (ScaleTime - elapsedTime)));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+    
+    //default values for fields
     private void Reset()
     {
+        //set default values for sprites
         NormalSprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
         HighlightedSprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
         PressedSprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
         DisabledSprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
 
+        //get file path of this script
         MonoScript monoScript = MonoScript.FromMonoBehaviour(this);
         string scriptPath = AssetDatabase.GetAssetPath(monoScript);
+        //get folder path
         scriptPath = scriptPath.Replace("ExtendedButton.cs", "");
+        //get Audio folder path
         string audioPath = scriptPath + "Audio/";
         
+        //set default values for audio clips
         HighlightClip = AssetDatabase.LoadAssetAtPath<AudioClip>(audioPath + "button_highlight.mp3");
         PressClip = AssetDatabase.LoadAssetAtPath<AudioClip>(audioPath + "button_press.mp3");
         DisabledPressClip = AssetDatabase.LoadAssetAtPath<AudioClip>(audioPath + "button_disabled.mp3");
     }
 
+    //Unity menu for adding this button
     [MenuItem("GameObject/UI/ExtendedButton",false,2002)]
     static void AddExtendedButton(MenuCommand menuCommand)
     {
+        //try to get GameObject if right click menu is used
         GameObject parent = menuCommand.context as GameObject;
+        //if there is no parent
         if (parent == null)
         {
+            //try to find a Canvas object in scene
             Canvas canvasInScene = FindObjectOfType<Canvas>();
+            //use found canvas as parent
             if (canvasInScene != null)
+            {
                 parent = canvasInScene.gameObject;
+            }
             else
             {
                 Debug.LogError("No parent object or Canvas found in scene!");
